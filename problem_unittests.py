f09975f7fd3feefc73c17bd9110e34c75c1ca008
Antonio Maradiaga <me@ajmaradiaga.com>
2017-06-01 16:14:55 +0100

    Added extra keywords to validate that it will works with pipes

--- problem_unittests.py
+++ problem_unittests.py
@@ -10,12 +10,12 @@ def _print_success_message():
 def test_create_lookup_tables(create_lookup_tables):
     with tf.Graph().as_default():
         test_text = '''
-        Moe_Szyslak Moe's Tavern Where the elite meet to drink
-        Bart_Simpson Eh yeah hello is Mike there Last name Rotch
+        Moe_Szyslak ||semicolon|| Moe's Tavern Where the elite meet to drink
+        Bart_Simpson: Eh yeah hello is Mike there Last name Rotch
         Moe_Szyslak Hold on I'll check Mike Rotch Mike Rotch Hey has anybody seen Mike Rotch lately
         Moe_Szyslak Listen you little puke One of these days I'm gonna catch you and I'm gonna carve my name on your back with an ice pick
-        Moe_Szyslak Whats the matter Homer You're not your normal effervescent self
-        Homer_Simpson I got my problems Moe Give me another one
+        Moe_Szyslak: Whats the matter Homer You're not your normal effervescent self
+        Homer_Simpson: I got my problems Moe Give me another one
         Moe_Szyslak Homer hey you should not drink to forget your problems
         Barney_Gumble Yeah you should only drink to enhance your social skills'''
 
